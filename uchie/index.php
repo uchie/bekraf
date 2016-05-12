2c446f77d0d1664194bd79f67a58539ad9377b38
uchie <shinyflavia87@gmail.com>
2016-05-12 15:19:06 +0700

    [uchie][project akhir]
    mengerjakan project akhir

---
+++ uchie/index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>being mother of 5 children</title>
+    <link rel="stylesheet" href="stylesheet.css" />
+</head>
+
+<body>
+    <div class="dropdown">
+        <span>Home</span>
+
+    </div>
+    <div class="dropdown">
+        <span>About</span>
+    </div>
+    <div class="dropdown">
+        <span>Menu</span>
+        <div class="dropdown-content">
+            <p>news</p>
+            <p>images</p>
+        </div>
+    </div>
+    
+    <div class="dropdown">
+        <span>contact</span>
+    </div>
+
+
+    <div style="background:silver">
+        <h1><u>Being mother of 5 children</u></h1>
+
+        <div class="picture">
+            <a href=""><img src="image/familypict2.jpg">
+            </a>
+            
+            <div class="socialmedia">
+                <a href=""><img src="facebook-icon.png"></a>
+                <a href=""><img src="twittericon.png"></a>
+            </div>
+        </div>
+    </div>
+
+    <p> 
+    <?php
+        echo "menjadi single mom dengan 5 orang anak itu susah-susah gampang tapi menyenangkan,kl di bayangin pasti ribet,rusuh,capek,,ga usah di bayangin deh kl gt hehe,,atau mau rasain skalian? eh jangan ah.punya anak dengan 5 anak yg beda usia dengan jarak yg deket itu ga gampang lho,d jamin rumah pasti berisik dan berantakan,tp beneran seru lho,,ya kl d seru2in sih,,hehe,,mau kenal ama anak2 ku??";
+    ?>
+    </p>
+    <div class="dropdown-content">
+        <p>Hello World!</p>
+        <p>profil</p>
+        <p>alamat</p>
+    </div>
+    <div style="backround:silver"></div>
+    <h2 style="color:purple;"><u>ini lah mreka</u></h2>
+    <ol>
+        <li><a href="#" class="myButton">M Alfredo</a>
+            <br><img src="http://4.bp.blogspot.com/-jnlY4Dre6oA/U5__q9I72xI/AAAAAAAAAiM/t1noGs_ECRs/s1600/Put_on_your_happy_face.png" /></li>
+
+        <li><a href="#" class="myButton">Siera May</a>
+            <br><img src="http://icons.iconarchive.com/icons/mat-u/camill/256/Filetype-png-icon.png" /></li>
+
+        <li><a href="#" class="myButton">Naysilla</a>
+            <br><img src="http://flyosity.com/images/_blogentries/networkicon/stepfinal2.png" /></li>
+
+        <li><a href="#" class="myButton">Gavin</a>
+            <br><img src="http://clipartfreefor.com/cliparts/files2/strawberry-clip-art-9Tpeo6gnc.png" /></li>
+
+        <li><a href="#" class="myButton">Flavia</a>
+            <br><img src="http://4.bp.blogspot.com/-jnlY4Dre6oA/U5__q9I72xI/AAAAAAAAAiM/t1noGs_ECRs/s1600/Put_on_your_happy_face.png" /></li>
+
+    </ol>
+    <p><strong> 
+    <?php
+        echo "my family my team ,be a good person children..mommy loves u";
+    ?>
+    </strong></p>
+
+    <h1><u>Being mother of 5 children</u></h1>
+
+    <div id="kolomkiri">
+        <?php
+           echo  "Menjadi ibu dr 5 org anak bukan lah cita-cita,namun sebuah takdir yg harus aku jalani,banyak org baru mendengar kalau aku seorang single mom dr 5 org anak saja mreka sudah geleng kepala,kok bisa,,pasti begitu anggapan mreka,pasti cape lah,pusing lah dan crita seram lain nya.Namun jujur memiliki anak 5 tidak se-stres perkiraan mreka,aku menikmati setiap proses nya,justru d setiap kericuhan yg terjadi d dlm rmh aku mendapatkan bnyk pelajaran,dan kami menjadi semakin sangat dekat 1 sama lain,,alhamdulillah slalu dgn kondisi spt sekarang ini";
+        ?>
+    </div>
+    <div id="kolomtengah">
+       <?php
+            echo    "Menjadi ibu dr 5 org anak bukan lah cita-cita,namun sebuah takdir yg harus aku jalani,banyak org baru mendengar kalau aku seorang single mom dr 5 org anak saja mreka sudah geleng kepala,kok bisa,,pasti begitu anggapan mreka,pasti cape lah,pusing lah dan crita seram lain nya.Namun jujur memiliki anak 5 tidak se-stres perkiraan mreka,aku menikmati setiap proses nya,justru d setiap kericuhan yg terjadi d dlm rmh aku mendapatkan bnyk pelajaran,dan kami menjadi semakin sangat dekat 1 sama lain,,alhamdulillah slalu dgn kondisi spt sekarang ini";
+        ?>
+    </div>
+    <div id="kolomkanan">
+        <?php
+              echo      "Menjadi ibu dr 5 org anak bukan lah cita-cita,namun sebuah takdir yg harus aku jalani,banyak org baru mendengar kalau aku seorang single mom dr 5 org anak saja mreka sudah geleng kepala,kok bisa,,pasti begitu anggapan mreka,pasti cape lah,pusing lah dan crita seram lain nya.Namun jujur memiliki anak 5 tidak se-stres perkiraan mreka,aku menikmati setiap proses nya,justru d setiap kericuhan yg terjadi d dlm rmh aku mendapatkan bnyk pelajaran,dan kami menjadi semakin sangat dekat 1 sama lain,,alhamdulillah slalu dgn kondisi spt sekarang ini";
+        ?>
+    </div>
+</body>
+
+</html>
---
+++ uchie/project/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Our Family</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet.css">
+
+</head>
+
+<body>
+    <div id="menu" class="container">
+        <div class="row">
+            <div id="menu" class="col-sm-12">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="header" class="container">
+        <div class="row">
+            <div id="banner" class="col-sm-12">
+                <img src="image/header.png">
+            </div>
+        </div>
+    </div>
+
+    <div id="content" class="container">
+        <div class="row">
+            <!--ini content utk menu tengah-->
+            <div id="content-utama" class="col-sm-8">
+                <h1>Perkenalan</h1>
+                <p>
+                    Menjadi single mom dengan 5 orang anak itu susah-susah gampang tapi menyenangkan,kl di bayangin pasti ribet,rusuh,capek,,ga usah di bayangin deh kl gt hehe,,atau mau rasain skalian? eh jangan ...punya 5 anak dengan beda usia yg deket itu ga gampang lho,d jamin rumah pasti berisik dan berantakan,tp beneran seru,,ya kl d seru2in sih,,hehe,,mau kenal ama anak2 ku??
+                </p>
+                <p>
+                    Anak yg pertama namanya Alfredo,panggilannya sih kk Al,badannya tinggi besar dgn tinggi 195cm,berat badan nyampe 100kg,dia skrng kls 2 SMK jurusan tekhnik mesin.
+                </p>
+                <p>
+                    Anak yg k 2 namanya Siera May,panggilannya kk Sie,tinggi badannya 170cm,berat badan 55kg,kk Sie skrng kls 2 SMP,kk Sie ini hoby nya dance,denger musik,paling seneng ikut berbagai macam lomba d skolah nya,,kk Sie paling item sendiri lho d rmh,hehe
+                </p>
+                <p>
+                    Anak k 3 namanya Naysilla,panggilannya kk Esil,kk Esil kls 1 SMP,rambutnya kriting sndiri d rmh,tinggi 173cm,berat 75kg tp suka ngaku 60kg,malu katanya kegedean,,kk esil ini yg paling sabar d rmh,seneng bgt ngurusin adek nya soalnya.
+                </p>
+                <p>
+                    Anak k 4 namanya Gavin Brava,panggilan gavin,gavin skrng kls 2 SD,kl gavin ini anak paling aktiv rmh,paling seneng ama binatang tp kl d suruh miara ga pernah bnr,mati smua.
+                </p>
+                <p>
+                    Anak k 5 namanya flavia Azalea,panggilannya Cia,cia paling mungil d rmh,paling centil,paling cerewet dan paling sgalanya,cia kls 1 SD,hobi nya ngaca,hehe..
+                </p>
+
+
+            </div>
+            <!--menu tengah selesai-->
+
+            <div id="content-link" class=col-sm-4>
+                <!--ini content utk link-->
+                <div id="isi-content-link">
+                    <h3> Artikel Saya </h3>
+                    <ul>
+                        <li><a href="artikel1.html">Tips Merawat Anak</a></li>
+                        <li><a href="">Tips Memasak Sarapan Praktis</a></li>
+                    </ul>
+                  
+                </div>
+            </div>
+            <!--content link selesai-->
+        </div>
+       
+
+
+    </div>
+    <div id="footer" class="container">
+        <div class="row">
+            <div id="footer" class="col-sm-12">
+                &copy; Uchie
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
